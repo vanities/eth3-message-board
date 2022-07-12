f50e95fc8cf149320c46af86c916a6b83954337f
Adamant Mischke <mischkeaa@gmail.com>
2022-07-11 22:35:46 -0500

    add prop types

--- components/messageForm.tsx
+++ components/messageForm.tsx
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import PropTypes from 'prop-types'
 
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
@@ -43,4 +44,9 @@ export class MessageForm extends Component {
       </Form>
     )
   }
+}
+
+MessageForm.propTypes = {
+  library: PropTypes.object,
+  onChange: PropTypes.func
 }
